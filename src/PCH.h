80ec40f410228e57f5ca460c13be998c498da6ca
yeahhowaboutnooo <62910188+yeahhowaboutnooo@users.noreply.github.com>
2022-02-13 17:48:46 +0100

    actual code

--- src/PCH.h
+++ src/PCH.h
@@ -1,10 +1,11 @@
 #pragma once
 
-#pragma warning(push)
-#include <RE/Skyrim.h>
-#include <REL/Relocation.h>
-#include <SKSE/SKSE.h>
+#include "RE/Skyrim.h"
+#include "SKSE/SKSE.h"
 
+#include <xbyak/xbyak.h>
+
+#pragma warning(push)
 #ifdef NDEBUG
 #	include <spdlog/sinks/basic_file_sink.h>
 #else
@@ -16,11 +17,6 @@ using namespace std::literals;
 
 namespace logger = SKSE::log;
 
-namespace util
-{
-	using SKSE::stl::report_and_fail;
-}
-
 #define DLLEXPORT __declspec(dllexport)
 
-#include "Plugin.h"
+#include "Version.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,93 @@
-namespace
+extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
 {
-	void InitializeLog()
-	{
 #ifndef NDEBUG
-		auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
+	auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
 #else
-		auto path = logger::log_directory();
-		if (!path) {
-			util::report_and_fail("Failed to find standard logging directory"sv);
-		}
-
-		*path /= fmt::format("{}.log"sv, Plugin::NAME);
-		auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
-#endif
-
-#ifndef NDEBUG
-		const auto level = spdlog::level::trace;
-#else
-		const auto level = spdlog::level::info;
-#endif
-
-		auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
-		log->set_level(level);
-		log->flush_on(level);
-
-		spdlog::set_default_logger(std::move(log));
-		spdlog::set_pattern("%g(%#): [%^%l%$] %v"s);
+	auto path = logger::log_directory();
+	if (!path) {
+		return false;
 	}
+
+	*path /= Version::PROJECT;
+	*path += ".log"sv;
+	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+#endif
+
+	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
+
+#ifndef NDEBUG
+	log->set_level(spdlog::level::trace);
+#else
+	log->set_level(spdlog::level::info);
+	log->flush_on(spdlog::level::warn);
+#endif
+
+	spdlog::set_default_logger(std::move(log));
+	spdlog::set_pattern("%g(%#): [%^%l%$] %v"s);
+
+	logger::info(FMT_STRING("{} v{}"), Version::PROJECT, Version::NAME);
+
+	a_info->infoVersion = SKSE::PluginInfo::kVersion;
+	a_info->name = Version::PROJECT.data();
+	a_info->version = Version::MAJOR;
+
+	if (a_skse->IsEditor()) {
+		logger::critical("Loaded in editor, marking as incompatible"sv);
+		return false;
+	}
+
+	const auto ver = a_skse->RuntimeVersion();
+	if (ver < SKSE::RUNTIME_1_5_39) {
+		logger::critical(FMT_STRING("Unsupported runtime version {}"), ver.string());
+		return false;
+	}
+
+	return true;
 }
-
-extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
-	SKSE::PluginVersionData v;
-
-	v.PluginVersion(Plugin::VERSION);
-	v.PluginName(Plugin::NAME);
-
-	v.UsesAddressLibrary(true);
-	v.CompatibleVersions({ SKSE::RUNTIME_LATEST });
-
-	return v;
-}();
 
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
 {
-	InitializeLog();
-	logger::info("{} v{}"sv, Plugin::NAME, Plugin::VERSION.string());
+	logger::info("loaded");
 
 	SKSE::Init(a_skse);
+
+	auto moduleBase = REL::Module::get().base();
+	auto injectionPoint = moduleBase + 0xc119d8;
+	auto &trampolin = SKSE::GetTrampoline();
+	trampolin.create(32, (void*)injectionPoint);
+
+	struct OrigCode : Xbyak::CodeGenerator
+	{
+		OrigCode()
+		{
+			mov(eax, qword[rax + 0x118]);
+		}
+	};
+	OrigCode orig;
+	orig.ready();
+
+	struct DeleteBit6 : Xbyak::CodeGenerator
+	{
+		DeleteBit6()
+		{
+			and_(cl, 0xdf); //delete bit #6
+		}
+	};
+	DeleteBit6 db6;
+	db6.ready();
+
+	trampolin.allocate(orig);
+	trampolin.allocate(db6);
+
+	//erase the original instruction with nops
+	std::vector<std::uint8_t> nops(orig.getSize());
+	std::fill(nops.begin(), nops.end(), 0x90);
+	REL::safe_write<std::uint8_t>(injectionPoint, nops);
+
+	//writes:
+	//jmp from injectionPoint -> start_InjectedCode
+	//jmp from end_InjectedCode -> injectionPoint+org.getSize()
+	trampolin.write_branch<5>(injectionPoint, injectionPoint+orig.getSize());
 
 	return true;
 }
